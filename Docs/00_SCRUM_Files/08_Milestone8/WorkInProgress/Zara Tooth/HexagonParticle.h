3df6e22d6e57f18cb4b976aa645114a3f9f64037
zaratooth <zara_tooth@carleton.ca>
2014-03-27 22:59:08 -0400

    adding my files to scrum folder

---
+++ Docs/00_SCRUM_Files/08_Milestone8/WorkInProgress/Zara Tooth/HexagonParticle.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "ofMain.h"
+#include "Particle.h"
+
+//static separation 
+static const float s_desiredSepSqrd = 20000;
+static const float s_neighDistSqrd = 90000;
+
+class HexagonParticle 
+	: public Particle {
+		
+public : 
+	ofColor hexColor;
+	float hexSize;
+	float hexAlpha;
+	float hexRotation;
+
+	bool isConnected;
+	float hexMaxSize;
+	float hexGrowthRate;
+	ofVec2f hexToHands;
+
+	HexagonParticle(ofVec3f _pos);
+	~HexagonParticle();
+
+	void update();
+	void separation(vector<HexagonParticle*>* connectedParticles);
+
+};
---
+++ Docs/00_SCRUM_Files/08_Milestone8/WorkInProgress/Zara Tooth/LightScene.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "IScene.h"
+#include "Light.h"
+#include "HexagonParticle.h"
+
+class LightScene : public IScene
+{
+	//for hex
+	ofImage m_hexImgBorder;
+	ofImage m_hexImgInner;
+	ofImage m_hexLineImg;
+
+	//for lights
+	ofImage m_lightImg;
+	ofImage m_lightTube;
+	vector<Light> m_lights;
+
+	//for background
+	ofImage m_backgroundImg;
+	ofSoundPlayer* pBackgroundSound;
+
+	//for fog
+	ofShader m_fogShader;
+	ofVbo m_fogVbo;
+	ofImage m_fogImg;
+	ofFbo m_fogAlphaMask;
+	ofImage m_lightAlpha;
+
+	//for hands
+	ofImage m_handsImage;
+	vector<Particle*> m_closestHand; 
+	Boolean m_connectedToHands;
+	vector<HexagonParticle*> m_connectedParticles;
+	
+public:
+	LightScene(vector<Particle*>* people, vector<Particle*>* hands, AudioManager* audioManager);
+
+	void Render();
+	void Update(float timeScale);
+
+	void onLoad();
+	void onUnload();
+
+	void convertPeopleVector();
+	Particle* addParticleOfProperType(ofVec3f _pos);
+
+	~LightScene();
+};
