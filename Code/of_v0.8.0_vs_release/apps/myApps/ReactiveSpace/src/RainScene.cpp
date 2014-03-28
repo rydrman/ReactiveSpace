4e30bfb66b23b9ec94eadf5868531e3193d21598
rydrman <bott0041@WT202-13W.ottawa.ad.algonquincollege.com>
2014-03-28 10:36:27 -0400

    rain particles rotating and changing color

--- Code/of_v0.8.0_vs_release/apps/myApps/ReactiveSpace/src/RainScene.cpp
+++ Code/of_v0.8.0_vs_release/apps/myApps/ReactiveSpace/src/RainScene.cpp
@@ -48,7 +48,7 @@ RainScene::RainScene(vector<Particle*>* people, vector<Particle*>* hands, AudioM
 		}
 	}
 
-	m_rainImage.loadImage("RainScene/Rain2.png");
+	m_rainImage.loadImage("RainScene/rainDropsWhite.png");
 	m_rainBackground.loadImage("RainScene/RainBackground.png");
 	m_cloudImage.loadImage("RainScene/Clouds_spreadsheet.png");
 	
@@ -72,7 +72,15 @@ void RainScene::Render()
 	{
 		ofPushMatrix();
 			ofTranslate(m_rainParticles[i].pos);
-			m_rainImage.draw(0.f, 0.f, 0.f, 15.f, 30.f);
+			float angle = atan2f(m_rainParticles[i].vel.y, m_rainParticles[i].vel.x);
+			ofRotate(ofRadToDeg(angle));
+			//grey 230 230 230
+			//blue 176, 231, 255
+			ofSetColor(ofMap(angle, -PI, PI, 230, 176), 
+					ofMap(angle, -PI, PI, 230, 231), 
+					ofMap(angle, -PI, PI, 230, 255));
+			m_rainImage.draw(-15.f, 7.5f, 0.f, 30.f, 15.f);
+			ofSetColor(255);
 		ofPopMatrix();
 	}
 
@@ -213,12 +221,16 @@ void RainScene::addNewRainDrop(Particle* p)
 	if (whichStart < ratio)
 	{
 		
-		if (m_createNextInCloudNum > pPeople->size()-1)
-			
+		if (m_createNextInCloudNum > pPeople->size() - 1)
+		{
 			m_createNextInCloudNum = 0;
-		RainCloudParticle* rc = (RainCloudParticle*)(pPeople->at(m_createNextInCloudNum));
-		rc->addRainDrop(p);
-		++m_createNextInCloudNum;
+		}
+		if (pPeople->size() > 0)
+		{
+			RainCloudParticle* rc = (RainCloudParticle*)(pPeople->at(m_createNextInCloudNum));
+			rc->addRainDrop(p);
+			++m_createNextInCloudNum;
+		}
 
 	}
 	else
